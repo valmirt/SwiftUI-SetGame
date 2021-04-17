1f605fbf5b49dc4673ae6a07e550e340a41d0075
valmirt <valmirt.dev@gmail.com>
2021-04-16 23:24:19 -0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Valmir Junior on 16/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
